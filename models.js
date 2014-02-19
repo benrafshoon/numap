d749e52cd65519bab20d260ff8fcc5e5944a13a5
Ben Rafshoon <benraf@u.northwestern.edu>
2014-02-18 21:06:36 -0600

    Named path schema, for named roads

--- models.js
+++ models.js
@@ -2,6 +2,7 @@ exports.initialize = function(mongoose) {
 	return function() {
 		var Schema = mongoose.Schema;
 		var edgeSchema = new Schema({
+			name: String,
 			travelType: String, //walk, bike, shuttle
 			schedulingType: String, //continuous (walk, bike), discrete (shuttle)
 			distance: Number, 
@@ -12,16 +13,22 @@ exports.initialize = function(mongoose) {
 		mongoose.model("Edge", edgeSchema);
 		
 		var nodeSchema = new Schema({
+			name: String,
 			latitude: Number,
 			longitude: Number
 		});
 		mongoose.model("Node", nodeSchema);
 		
+		var namedPathSchema = new Schema({
+			name: String,
+			edges: [Schema.Types.ObjectId] //Set of edges that define a path (ie Sheridan Road)
+		});
+		mongoose.model("NamedPath", namedPathSchema);
+		
 		var shuttleStopSchema = new Schema({
 			shuttleName: String,
 			postedStopTime: Date
 			//Eventually will have a set of actual times to compute average and worst case (ie 99 percentile) times
-			
 		});
 		mongoose.model("Shuttle", shuttleStopSchema);
 	};
